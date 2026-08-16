757f811272b2cbcef4fac53c99f5312a4b1ac3f3
Penguin <haydenpersonal@proton.me>
2026-08-16 14:50:29 -0500

    links and new pfp

--- scripts/manifest.js
+++ scripts/manifest.js
@@ -2,10 +2,17 @@
 // re-run `node build-manifest.js` after adding or removing files in /images
 window.PHOTO_MANIFEST = [
   {
-    "id": "active-2026-08-06",
-    "file": "avatars/active-2026-08-06.webp",
+    "id": "2026-08-06",
+    "file": "avatars/2026-08-06.webp",
     "frame": "001",
     "dateAdded": "2026-08-06",
+    "defaultActive": false
+  },
+  {
+    "id": "active-2026-08-16",
+    "file": "avatars/active-2026-08-16.webp",
+    "frame": "002",
+    "dateAdded": "2026-08-16",
     "defaultActive": true
   }
 ];
