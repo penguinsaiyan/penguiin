1fab8708066161a21c6e599a46fb635c09ac58b1
Penguin <haydenpersonal@proton.me>
2026-08-20 21:27:18 -0500

    NEW AVATAR FFS

--- scripts/manifest.js
+++ scripts/manifest.js
@@ -9,10 +9,17 @@ window.PHOTO_MANIFEST = [
     "defaultActive": false
   },
   {
-    "id": "active-2026-08-16",
-    "file": "avatars/active-2026-08-16.webp",
+    "id": "2026-08-16",
+    "file": "avatars/2026-08-16.webp",
     "frame": "002",
     "dateAdded": "2026-08-16",
+    "defaultActive": false
+  },
+  {
+    "id": "active-2026-08-20",
+    "file": "avatars/active-2026-08-20.jpg",
+    "frame": "003",
+    "dateAdded": "2026-08-21",
     "defaultActive": true
   }
 ];
